d5b80668d2956fac9ed812722c366466a11fd12f
Coleman Kelleghan <imdesigner2012@gmail.com>
2022-04-04 22:11:11 -0600

    Menu

--- src/components/KnightMoves.tsx
+++ src/components/KnightMoves.tsx
@@ -1,16 +1,7 @@
-import React from 'react';
+import React, { ReactNode } from 'react'
 
-export const KnightMoves = () => {
-  const test = async () => {
-    await Promise.resolve({done: true})
-    .then(res => {
-      console.log(res)
-    })
-  }
-
-  test()
-
+export const KnightMoves = (): ReactNode => {
   return (
     <div>{'react setup from scratch without cra testing ts'}</div>
-  );
+  )
 }
